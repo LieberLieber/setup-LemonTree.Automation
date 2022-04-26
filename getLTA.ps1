7d2093d604d3bdfdd224573b8bcc9ac8d2299ef4
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 21:06:30 +0200

    Initial Commit

---
+++ getLTA.ps1
@@ -0,0 +1,22 @@
+#getLTA.ps1
+param(
+    [parameter(Mandatory = $true)]
+    [string]$LemonTreePackageURL,
+
+    [parameter(Mandatory = $false)]
+    [string]$License
+)
+
+echo "Download LemonTree.Automtion from Repo"
+while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
+curl "$LemonTreePackageURL" --output LTA.zip -k
+Expand-Archive LTA.zip -DestinationPath .\LTA\ -Force
+
+IF([string]::IsNullOrWhiteSpace($License)) {            
+    echo "No License info provided."         
+} else {            
+    echo "Create License File from provided info"
+    $License | Out-File -FilePath lta.lic #if you deploy the license on the fly
+}  
+
+echo '::set-output name=LemonTreeAutomationExecutable::.\LTA\LemonTree.Automation.exe'
