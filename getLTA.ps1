93595423e361408fc763d4bd29f881b656d0f620
Daniel Siegl <41949368+danielsiegl@users.noreply.github.com>
2023-10-24 09:12:22 +0200

    Create a Test Action to run on Ubuntu (#5)
    LemonTree 4.0 Version of Action with support for Ubuntu and Windows

--- getLTA.ps1
+++ getLTA.ps1
@@ -1,32 +1,55 @@
 #getLTA.ps1
 param(
-    [parameter(Mandatory = $true)]
-    [string]$LemonTreePackageURL,
-
     [parameter(Mandatory = $false)]
-    [string]$License
+    [string]$License,
+    [parameter(Mandatory = $false)]
+    [string]$RunnerOs 
 )
 
-echo "Download LemonTree.Automtion from Repo"
+if ($RunnerOs -eq 'Linux') 
+{
+    $LemonTreePackageURL = "https://nexus.lieberlieber.com/repository/lemontree-release/LemonTree.Automation/LemonTree.Automation.Linux.Zip_latest.zip"
+}
+elseif ($RunnerOs -eq 'Windows') 
+{
+    $LemonTreePackageURL  = "https://nexus.lieberlieber.com/repository/lemontree-release/LemonTree.Automation/LemonTree.Automation.Windows.Zip_latest.zip"
+}
+else 
+{
+    Write-Output '$RunnerOs is not supported'
+    Exit -1
+}
+
+Write-Output "Download LemonTree.Automtion from Repo"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
 curl "$LemonTreePackageURL" --output LTA.zip -k
 Expand-Archive LTA.zip -DestinationPath .\LTA\ -Force
 
 IF([string]::IsNullOrWhiteSpace($License)) 
 {            
-    echo "No License info provided."         
+    Write-Output "No License info provided."         
 } 
 else 
 {            
-    echo "Create License File from provided info"
+    Write-Output "Create License File from provided info"
     $License | Out-File -FilePath lta.lic #if you deploy the license on the fly
 }  
 
 if($LASTEXITCODE -eq 0)
 {
-    $version = .\LTA\LemonTree.Automation.exe Version
-    echo "LieberLieber LemonTree.Automation Version: $version is ready."
-    echo 'LemonTreeAutomationExecutable=.\LTA\LemonTree.Automation.exe' >> $env:GITHUB_OUTPUT
+
+    if ($RunnerOs -eq 'Linux') 
+    {
+        $LemonTreeExe = "./LTA/LemonTree.Automation"
+        #workaround because github artifacts logic doesn't maintain properties
+        chmod +x $LemonTreeExe            
+    }
+    elseif ($RunnerOs -eq 'Windows') 
+    {
+        $LemonTreeExe  = ".\LTA\LemonTree.Automation.exe"
+    }
+
+    Write-Output "LemonTreeAutomationExecutable=$LemonTreeExe" >> $env:GITHUB_OUTPUT
     #workaround because LemonTree.Automation.exe Version provides misleading Exitcode
     exit 0
 }
